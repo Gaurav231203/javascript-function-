43d5fce981ac694f28bc034351b86f5b69f489ca
gaurav <bishtgaurav222@gmail.com>
2026-06-25 02:49:31 +0530

    added javascript file

---
+++ firstclassfunc.js
@@ -0,0 +1,18 @@
+//first class function means function ko hum variable kay andar store kar sakte hai or usko hum as a parameter bhej sakte hai or return bhi kar sakte hai
+//or hum function ko values ki tarah use kar sakte hai
+ function abc(val){
+     
+    val();
+    
+ }
+ abc(function()
+ {console.log("Hello, World!");
+
+ });
+
+/* function add( v1, v2){
+ v1(v2);
+ 
+ }
+ add(20 ,30,function()
+ {console.log("Adding values!");});*/
