084097a48ef6095cc91c4cc1509a62aa76bad195
gaurav <bishtgaurav222@gmail.com>
2026-06-25 02:15:23 +0530

    added javascript file

---
+++ return.js
@@ -0,0 +1,19 @@
+//return means function kay andar say koi  value vapis bhejna  
+// matlab function nay jo kaam kiya hai usak result bahr bhrejna
+
+/*function add(v1,v2){
+    return v1 + v2;
+}
+console.log(add(10,20));*/
+
+
+
+/*let add =function(v1,v2){
+    return v1 + v2;
+}
+console.log(add(10,20));*/
+
+let add=(v1,v2)=>{
+    return v1 + v2;
+}
+console.log(add(10,20));
