ffdf6aa695be62f24bddae3bac2aed8f0b5f24ea
gaurav <bishtgaurav222@gmail.com>
2026-06-25 00:44:19 +0530

    added javascript file

---
+++ parametr.js
@@ -0,0 +1,14 @@
+//dynamiic value  function
+/*let fun1=(v1)=>{
+    console.log(`${v1} nach rha hai`);
+}
+fun1("Raju");
+fun1("Rani");
+fun1("Ramu");
+*/
+function fun(v1){
+    console.log(`${v1} nach rha hai`);
+}
+fun("Raju");
+fun("Rani");
+fun("Ramu");
