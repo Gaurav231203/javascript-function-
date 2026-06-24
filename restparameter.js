d724dd733cdc9ae38d8016721c773dbf76718046
gaurav <bishtgaurav222@gmail.com>
2026-06-25 01:29:22 +0530

    added javascript file

--- restparameter.js
+++ restparameter.js
@@ -4,7 +4,13 @@
 // agar ... kisi bhi function kay parameter kay aage lagaya jaye to ye  rest parameter ban jata hai or ye function kay andar array ki tarah kaam karta hai 
 //agar wo array and object  mai lage ...  to ye spread operator ban jata hai or ye array or object ko spread kar deta hai
 
-function add(...gauri){
+/*function add(...gauri){
 console.log(gauri);
 }
-add(1,2,3,4,5,6,7,8,9,10); 
+add(1,2,3,4,5,6); */
+
+// on emore way to do it
+function add(a,b,c , ...val){
+console.log(a,b,c,val);
+}
+add(1,2,3,4,5,6); 
