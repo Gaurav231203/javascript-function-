bcd014bb6058c69e926b3c6c487a64d7fb058df4
gaurav <bishtgaurav222@gmail.com>
2026-06-25 00:58:11 +0530

    added javascript file

--- moreparameter.js
+++ moreparameter.js
@@ -1,6 +1,9 @@
+//function kay name kay bracket andar hum parameters pass karte hai or ye parameters function kay andar variables ki tarah kaam karte hai
+
 function add(b1,b2){
     console.log(b1 + b2);
 }
+//jha pr function word nahi hota hai or hum real value bhejte hai usko arguments bolte hai
 add(20,30);
 add(50,60);
     add(100,200);
