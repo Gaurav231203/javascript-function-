31e81f68ffa1256bdd59942665239477bb9e17ea
gaurav <bishtgaurav222@gmail.com>
2026-06-25 01:25:04 +0530

    added javascript file

--- restparameter.js
+++ restparameter.js
@@ -1,6 +1,9 @@
 //jab bahut sare argument ho to hume utnehe parameter pass karne ki jarurat 
 // hai jitne hum chahte hai or  issaye hum  bachne  kay liye  rest parameter ki help se kate hai
 // (... ise rest or spread operator bolte hai)
+// agar ... kisi bhi function kay parameter kay aage lagaya jaye to ye  rest parameter ban jata hai or ye function kay andar array ki tarah kaam karta hai 
+//agar wo array and object  mai lage ...  to ye spread operator ban jata hai or ye array or object ko spread kar deta hai
+
 function add(...gauri){
 console.log(gauri);
 }
