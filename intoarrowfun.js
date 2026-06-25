18d3d7b31a2184cf39a51f734c6447a75d39c1b2
gaurav <bishtgaurav222@gmail.com>
2026-06-25 17:57:24 +0530

    added javascript file

---
+++ intoarrowfun.js
@@ -0,0 +1,9 @@
+//convert normal function into arrow function
+/*function multiply(a,b){
+    return a*b;
+}
+console.log (multiply(12,2));*/
+let multiply= (a,b)=>{
+    return a*b;
+}
+console.log(multiply(12,2))
