445bf47c9d1dcc12bf103d0e6b407770034c53fc
gaurav <bishtgaurav222@gmail.com>
2026-06-25 17:04:49 +0530

    added javascript file

---
+++ hoisting.js
@@ -0,0 +1,8 @@
+/* means ki hum kisi bhi function or vvariable ko  use krne say phle usko declare
+ krr sakte hai or usko use bhi krr sakte hai or ye sirf function or variable ke liye hi hota hai
+  or ye function ke andar hi hota hai*/
+
+  console.log(abc(10,20)); //undefined
+  function abc(a,b){
+    return a+b;
+  }
