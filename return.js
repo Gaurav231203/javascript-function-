6f297a48344eb2dbf61648849350489fc88bd3f1
gaurav <bishtgaurav222@gmail.com>
2026-06-25 11:55:47 +0530

    added javascript file

--- return.js
+++ return.js
@@ -13,7 +13,14 @@ console.log(add(10,20));*/
 }
 console.log(add(10,20));*/
 
-let add=(v1,v2)=>{
+/*let add=(v1,v2)=>{
     return v1 + v2;
 }
-console.log(add(10,20));
+console.log(add(10,20));*/
+
+function add(){
+   return  12;
+
+}
+let val=add();
+console.log(val);
