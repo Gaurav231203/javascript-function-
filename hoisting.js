7a21ce2d586af8cb0b96ade202153d28d2ae50cf
GAURAV BISHT <bishtgaurav222@gmail.com>
2026-07-14 13:51:34 +0530

    Added new files

--- hoisting.js
+++ hoisting.js
@@ -2,10 +2,10 @@
  krr sakte hai or usko use bhi krr sakte hai or ye sirf function or variable ke liye hi hota hai
   or ye function ke andar hi hota hai*/
 
- /* console.log(abc(10,20)); //undefined
+  console.log(abc(10,20)); //undefined
   function abc(a,b){
     return a+b;
-  }*/
+  }
 //hoisting  works only in declaration 
 //but it wont work in fucntion expression or variable expression
 // so hoisting is only for function declaration and variable declaration 
